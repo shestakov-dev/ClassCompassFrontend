7f9539213974bc744e6f4d33bb1b274d2f148527
Alexander Shestakov <alexandurshestakov@gmail.com>
2025-12-17 14:30:36 +0200

    initialize shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,4 +15,9 @@ export default defineConfig({
 			optimize: true,
 		}),
 	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
