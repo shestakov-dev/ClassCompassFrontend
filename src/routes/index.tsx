7bbdea5d43947cf4928cadd8b88bb3f6f91dca19
Alexander Shestakov <alexandurshestakov@gmail.com>
2025-12-18 22:17:21 +0200

    removed unused files leftover from the merge

--- src/routes/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function IndexPage() {
-	return (
-		<div>
-			<h1 className="text-xl font-bold">Welcome to ClassCompass</h1>
-			<p className="mt-2">Please log in to continue.</p>
-		</div>
-	);
-}
