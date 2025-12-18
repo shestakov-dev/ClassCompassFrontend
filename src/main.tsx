34f18d792880d2264017dfa46dabdd24175bc26d
Alexander Shestakov <alexandurshestakov@gmail.com>
2025-12-18 22:09:20 +0200

    initialized the project using Vite, React and Shadcn UI with a custom theme

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "@/index.css";
+import { RouterProvider } from "react-router";
+import { router } from "@/router";
+import { ThemeProvider } from "@/context/theme-context";
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<ThemeProvider>
+			<RouterProvider router={router} />
+		</ThemeProvider>
+	</StrictMode>
+);
