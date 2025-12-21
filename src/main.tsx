9c3e3aef30958edc403f414370eb678c3595cc6d
Alexander Shestakov <alexandurshestakov@gmail.com>
2025-12-21 16:06:08 +0200

    using orval to generate services based on OpenAPI schema from server; initiated tanstack query in react

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,28 @@ import { RouterProvider } from "react-router";
 import { router } from "@/router";
 import { ThemeProvider } from "@/context/theme-context";
 import { LoadingProvider } from "@/context/loading-context";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			retry: 1,
+			refetchOnWindowFocus: false,
+		},
+	},
+});
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<ThemeProvider>
-			<LoadingProvider>
-				<RouterProvider router={router} />
-			</LoadingProvider>
-		</ThemeProvider>
+		<QueryClientProvider client={queryClient}>
+			<ThemeProvider>
+				<LoadingProvider>
+					<RouterProvider router={router} />
+				</LoadingProvider>
+			</ThemeProvider>
+
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	</StrictMode>
 );
